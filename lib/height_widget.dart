77df014dd29ddc0302c9213e762ecf0d6649a1e9
20192726 <20192726@fue.edu.eg>
2023-03-28 16:01:08 +0200

    Initial commit

---
+++ lib/height_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HeightWidget extends StatefulWidget {
+  final Function(int) onChange;
+  const HeightWidget({Key? key, required this.onChange}) : super(key: key);
+
+  @override
+  State<HeightWidget> createState() => _HeightWidgetState();
+}
+
+class _HeightWidgetState extends State<HeightWidget> {
+
+  int _height = 150;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+          elevation: 12,
+          shape: RoundedRectangleBorder(),
+      child: Column(
+        children: [
+          Text("Height",style: TextStyle(fontSize: 25, color: Colors.blueAccent,fontWeight: FontWeight.w500),),
+          SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(_height.toString(),style: TextStyle(fontSize: 40,color: Colors.blueAccent,fontWeight: FontWeight.w500),),
+              SizedBox(width: 10,),
+              Text("cm",style: TextStyle(fontSize: 20,color: Colors.black,),),
+            ],
+          ),
+          Slider(
+              min:0,
+              max: 240,
+              value: _height.toDouble(),
+              thumbColor: Colors.blueAccent,
+              onChanged: (value){
+                setState(() {
+                  _height = value.toInt();
+                });
+                widget.onChange(_height);
+              } ,
+          ),
+        ],
+      )
+      ),
+    );
+  }
+}
